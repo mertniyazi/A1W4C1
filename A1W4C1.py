4835bc7471305ba6e2f9e2c42850397f8f4fa8f2
mertniyazi <95623155+mertniyazi@users.noreply.github.com>
2024-09-30 12:13:30 +0200

    Update A1W4C1.py

--- A1W4C1.py
+++ A1W4C1.py
@@ -1,17 +1,23 @@
 def rechtzaak():
-    ...
+    print("a")
+    keuze = input("druk iets om verder tegaan: ")
+    print("m")
+    return keuze
 
 
 def gevangenis():
-    ...
+    print("b")
 
 
 def kantine():
-    ...
+    print("c")
 
 
 def buiten():
-    ...
+    print("d")
 
 
-print("hallo")
+while True:
+    if rechtzaak() == "accepteren":
+        gevangenis()
+        break
