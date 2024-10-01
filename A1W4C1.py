ee696e8db9b08d30b92ed32a1744d24c8da5f63e
mertniyazi <95623155+mertniyazi@users.noreply.github.com>
2024-10-01 10:53:31 +0200

    Update A1W4C1.py

--- A1W4C1.py
+++ A1W4C1.py
@@ -1,18 +1,40 @@
+import time
+
 
 def rechtzaak():
-    keuze_rechtzaak = input("druk iets om verder tegaan: ")
-
-    return keuze_rechtzaak
+    keuze_rechtzaak = input(
+        "typ 1 van de 3 opties(accepteren, weg rennen of liegen): ").lower()
+    if keuze_rechtzaak == "accepteren":
+        print(f"Je hebt jou daad geaccepteerd er krijgt 20 jaar celstraf van het beroven van een oude vrouw.")
+        return True
+    elif keuze_rechtzaak == "weg rennen":
+        print(f"Je probeert weg te rennen als een mogool en wordt getackeld door een politie agent.")
+        return True
+    elif keuze_rechtzaak == "liegen":
+        print(f"Omdat je zat te liegen tijdens een serieuze rechtzaak wordt je veroordeeld voor maximaal levenseis en je kan er nooit meer eruit.")
+        return False
+    else:
+        print(f"Voer een valide keuze in.")
 
 
 def gevangenis():
-    keuze_gevangenis = input("Voer een keuze in: ")
-    if keuze_gevangenis == "Drillboor":
-        print("Je valt in de politie bureau")
+    keuze_gevangenis = input(
+        "typ 1 van de 3 opties(accepteren, weg rennen of liegen): ").lower()
+    if keuze_gevangenis == "accepteren":
+        print(f"Je hebt jou daad geaccepteerd er krijgt 20 jaar celstraf van het beroven van een oude vrouw.")
+        return True
+    elif keuze_gevangenis == "weg rennen":
+        print(f"Je probeert weg te rennen als een mogool en wordt getackeld door een politie agent.")
+        return True
+    elif keuze_gevangenis == "liegen":
+        print(f"Omdat je zat te liegen tijdens een serieuze rechtzaak wordt je veroordeeld voor maximaal levenseis en je kan er nooit meer eruit.")
+        return False
+    else:
+        print(f"Voer een valide keuze in.")
 
 
 def kantine():
-    print("c")
+    keuze_kantine = input("")
 
 
 def buiten():
