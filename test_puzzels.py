e3dee785c1bf515219790fad17f12b31fe2864e1
mertniyazi <95623155+mertniyazi@users.noreply.github.com>
2024-10-02 14:17:43 +0200

    Create test_puzzels.py

---
+++ test_puzzels.py
@@ -0,0 +1,11 @@
+import time
+
+
+def puzzel_1():
+    # print("""\
+
+    #                 """)
+    print("omdat we nu bij de onderdeel van een rechtzaak zijn")
+    print("Gaan we jou knowledge teste of je uberhaubt de weten weet van Nederland")
+
+    antwoord = input("")
