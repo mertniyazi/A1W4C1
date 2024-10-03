00e8df0806e607fbaa49fd4a34960fa97cbefc11
mertniyazi <95623155+mertniyazi@users.noreply.github.com>
2024-10-03 10:36:05 +0200

    Update test_puzzels.py

--- test_puzzels.py
+++ test_puzzels.py
@@ -10,12 +10,15 @@ def puzzel_1():
     time.sleep(2)
     print('''Waarom is nederland een rechtsstaat
     - (1)Iedereen moet zich aan de wet houden 
-    - (2)
+    - (2)Omdat 
     - (3)Nederland is geen rechtstaat maar een  land
     Kies: ''')
-    antwoord_puzzel_1 = int(input('Voer 1, 2 of 3 in'))
+    antwoord_puzzel_1 = int(input('Voer 1, 2 of 3 in: '))
 
     if antwoord_puzzel_1 == 1:
         return print("Dat is correct iedereen is gelijk volgens de wet")
     elif antwoord_puzzel_1 == 2:
-        print("")
+        return print("yoyoyo")
+
+
+puzzel_1()
